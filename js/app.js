7e005a3fc4e810f1160d6ec0595eeeb5f5afb3a9
jtabach <jeff.n.tabachnick@gmail.com>
2017-03-18 23:49:53 -0700

    refactor: move common functions out of viewmodel

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 var map;
 
-// yelp pizza query
-// https://api.yelp.com/v2/search/?location=San Francisco, CA&category_filter=pizza
-
 var initialLocations = [
   { index: 0, title: 'Katani Pizza', location: { lat: 37.773193359375,  lng: -122.450645446777 } },
   { index: 1, title: 'Slice House By Tony Gemignani', location: { lat: 37.7696380615234, lng: -122.447540283203 } },
@@ -24,13 +21,7 @@ function initMap() {
   for (var i = 0; i < initialLocations.length; i++) {
     var position = initialLocations[i].location;
     var title = initialLocations[i].title;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      icon: 'https://www.google.com/mapfiles/marker_orange.png',
-      id: i
-    });
+    var marker = createMarker(map, position, title, i);
     markers.push(marker);
     bounds.extend(marker.position);
     var largeInfoWindow = new google.maps.InfoWindow();
@@ -41,6 +32,22 @@ function initMap() {
   map.fitBounds(bounds);
 }
 
+function createMarker(map, position, title, i) {
+  return new google.maps.Marker({
+    map: map,
+    position: position,
+    title: title,
+    icon: 'https://www.google.com/mapfiles/marker_orange.png',
+    id: i
+  });
+}
+
+function resetMarkerColors(markers) {
+  markers.forEach(function(marker) {
+    marker.setIcon('https://www.google.com/mapfiles/marker_orange.png');
+  });
+}
+
 function populateInfoWindow(marker, infowindow) {
     infowindow.marker = marker;
     infowindow.setContent('<div>' + marker.title + '</div>');
@@ -48,14 +55,36 @@ function populateInfoWindow(marker, infowindow) {
     infowindow.addListener('closeclick', function() {
       infowindow.setPosition(null);
     });
-    markers.forEach(function(marker) {
-      marker.setIcon('https://www.google.com/mapfiles/marker_orange.png');
-    });
+    resetMarkerColors(markers);
     marker.setAnimation(google.maps.Animation.BOUNCE);
     marker.setIcon('https://www.google.com/mapfiles/marker_green.png');
-    setTimeout(function() {
-      marker.setAnimation(google.maps.Animation.null);
-    },1400);
+    stopAnimation(marker);
+}
+
+function stopAnimation(marker) {
+  setTimeout(function() {
+    marker.setAnimation(google.maps.Animation.null);
+  }, 1400);
+}
+
+function resetMarkers() {
+  for (var i=0; i < markers.length; i++) {
+      markers[i].setMap(null);
+  }
+  markers = [];
+}
+
+function setMarkers(locations) {
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].location;
+    var title = locations[i].title;
+    var marker = createMarker(map, position, title, i);
+    var largeInfoWindow = new google.maps.InfoWindow();
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfoWindow);
+    });
+    markers.push(marker);
+  }
 }
 
 function AppViewModel() {
@@ -69,53 +98,17 @@ function AppViewModel() {
       return loc.title.match(self.pizzaFilter());
     });
     self.locs(filtered);
-    this.resetMarkers();
-    this.setMarkers(filtered);
+    resetMarkers();
+    setMarkers(filtered);
   }
 
-  this.resetMarkers = function() {
-    for (var i=0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }
-    markers = [];
-  }
-
-  this.setMarkers = function(locations) {
-    for (var i = 0; i < locations.length; i++) {
-      var position = locations[i].location;
-      var title = locations[i].title;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        icon: 'https://www.google.com/mapfiles/marker_orange.png',
-        id: i
-      });
-      var largeInfoWindow = new google.maps.InfoWindow();
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfoWindow);
-      });
-      markers.push(marker);
-    }
-  }
-
-  this.setAllMarkersRed = function(markers) {
-    markers.forEach(function(marker) {
-      marker.setIcon('https://www.google.com/mapfiles/marker_orange.png');
-    });
-  }
-
-  this.locationClick = function(location) {
-    self.setAllMarkersRed(markers);
+  self.locationClick = function(location) {
+    resetMarkerColors(markers);
     google.maps.event.trigger(markers[location.index], 'click');
-    setTimeout(function() {
-      markers[location.index].setAnimation(google.maps.Animation.null);
-    },1400);
+    stopAnimation(markers[location.index]);
   }
 
-
-
-  this.showListView = ko.observable(true);
+  self.showListView = ko.observable(true);
 }
 
 ko.applyBindings(new AppViewModel());
